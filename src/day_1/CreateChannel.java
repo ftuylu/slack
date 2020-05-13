b5f3a8fb21469b1445ed58df6efbf3343ff5a806
Fatma Tuylu <62037756+ftuylu@users.noreply.github.com>
2020-05-13 15:59:10 +0100

    Update CreateChannel.java

--- src/day_1/CreateChannel.java
+++ src/day_1/CreateChannel.java
@@ -3,6 +3,13 @@ package day_1;
 public class CreateChannel {
     public static void main(String[] args) {
         System.out.println("User is able to create a channel");
-
+        int x=0;
+if (x==0){
+    System.out.println("equal");
+}
+int x=0;
+if (x==0){
+    System.out.println("equal");
+}
     }
 }
