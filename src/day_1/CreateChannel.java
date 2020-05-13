d4f92f0de93a7c8984cb240444014bac2828262c
ftuylu <ftavus@yahoo.com>
2020-05-13 15:58:01 +0100

    update CreateChannel.Java

--- src/day_1/CreateChannel.java
+++ src/day_1/CreateChannel.java
@@ -3,6 +3,9 @@ package day_1;
 public class CreateChannel {
     public static void main(String[] args) {
         System.out.println("User is able to create a channel");
-
+        String s= "a";//changed from intelliJ
+        if(s.contains("a")){
+            System.out.println("contains");
+        }else System.out.println("not");
     }
 }
